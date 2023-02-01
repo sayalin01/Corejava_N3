6e3472921a931c258f11f5a4fae8224817aa3c65
Dell <Dell@Prasad>
2023-02-01 13:57:20 +0530

    First Commit

---
+++ dayone/src/org/tnsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,11 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WElcome to java fullstack program");
+
+	}
+
+}
